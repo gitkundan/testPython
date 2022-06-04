fc4f56d890134a9c63915cc1bfab038a6fa8d655
gitkundan <37186181+gitkundan@users.noreply.github.com>
2022-06-04 14:39:45 +0100

    Update snippets.py

--- snippets.py
+++ snippets.py
@@ -46,3 +46,46 @@ with open(filename,'r') as f:
     lines=f.readlines()
     print(lines)
 
+#reading archive files (zip)
+#remember to save the intenet requested get file on local before calling zip
+# Import the required method
+from zipfile import ZipFile
+# path: /tmp/dependencies/data/source/downloaded_at=2021-02-01/PPR-ALL.zip
+
+with ZipFile(path, "r") as f:
+    # Get the list of files
+    file_names = f.namelist()
+    print(file_names)
+    # Extract the CSV file
+    csv_file_path = f.extract(file_names[0])
+    print(csv_file_path)
+
+#reading / writing csv
+import csv
+from pprint import pprint as print
+filename="C:/Users/Dell/Downloads/PPR-2022-01-Meath(1).csv"
+
+with open(filename,'r') as f:
+    reader=csv.DictReader(f)
+    row=next(reader)
+    print(row)
+
+#write to csv
+import csv
+
+with open(path, mode="r", encoding="windows-1252") as reader_csv_file:
+    reader = csv.DictReader(reader_csv_file)
+    # The new file is called "PPR-2021-Dublin-new-headers.csv"
+    # and will be saved inside the "tmp" folder    
+    with open("/tmp/PPR-2021-Dublin-new-headers.csv",
+                    mode="w",
+                    encoding="windows-1252",
+                ) as writer_csv_file:
+        writer = csv.DictWriter(writer_csv_file, fieldnames=new_column_names)
+        # Write header as first line
+        writer.writerow(new_column_names)
+        for row in reader:
+	        # Write all rows in file
+	        writer.writerow(row)
+
+            
