d31594aa62332c459b95b76eba07b4550b94b187
gitkundan <37186181+gitkundan@users.noreply.github.com>
2022-11-04 07:42:03 +0000

    Update TestPython.py

--- TestPython.py
+++ TestPython.py
@@ -1,14 +1,34 @@
-'''
-text="""
-concept '{Account/{Financial Account, Transaction Account},Rate,Request/{Sales, Order}}'
-"""
-text=text.strip()
-import re
-# s = "Account\Trade"
-pattern_1="^concept '([\w\W]+)'" #match all word and non-word multiple times
-pattern_2="[^\/{},]+" #match all word and non-word multiple times
+from functools import reduce
+import operator
+import numpy as np
+
+text='Boris' #case 1
+text='{Boris,Rishi}' #case 2
+text='{UK/{Boris,Rishi},US/{Biden,Trump}}' #case 3
+# text='{UK/{Boris,Rishi}}' #case 3
 
 
-for record in re.findall(pattern_1, text):
-    x=re.findall(pattern_2,record)
-    print(x)
+text=text.strip("'").removesuffix('}').removeprefix('{')
+if text.find('/') >0 :
+    #identifier for case 3
+    text=text.split('}') #=> ('UK', '/{', 'Boris,Rishi')
+    for i in text:
+        #start of each parent block with associated childs
+        if len(i)>0:
+            value=i.partition('/{')
+            concept_parent=value[0]
+            concept_childs=value[2].split(',') #=> list : ['Boris', 'Rishi']
+            # out = reduce(operator.concat, concept_childs)
+            # out=list(np.array(concept_childs).flat)
+            result=[f'{concept_parent}/{i}' for i in concept_childs]
+            print(result)
+            # print(concept_childs)
+            # print(concept_parent)
+            # concept_childs #=> ['Boris,Rishi']
+            # print(concept_childs) #=> ['Boris,Rishi']
+            # for i in concept_childs:
+            #     x=f'{concept_parent}/{i}'
+            #     print(i)
+
+
+
