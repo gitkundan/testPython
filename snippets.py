57c16126db007b55313a007f65bc4c2a40707c55
gitkundan <37186181+gitkundan@users.noreply.github.com>
2023-03-23 06:47:02 +0000

    Update snippets.py

--- snippets.py
+++ snippets.py
@@ -168,6 +168,54 @@ df.loc[condition,'reward']=df['favourite_food']
 
 
 
+========================================================================================
+# Add file path to Windows PATH
+import os
+mingw_path = r'C:\mingw-w64\mingw64\bin'
+os.environ['PATH']=mingw_path + ';' + os.environ['PATH']
+print(os.environ['PATH'])
+
+========================================================================================
+# Generate mock data
+from faker import Faker
+
+fake = Faker()
+
+person = {}
+# person['id'] = fake.ssn()
+person = type(random.randrange(1,9))
+person=range(11,14)
+person=random.randint(11,14)
+# person['first_name'] = fake.first_name()
+# person['last_name'] = fake.last_name()
+# person['email'] = fake.unique.ascii_email()
+# person['company'] = fake.company()
+# person['phone'] = fake.phone_number()
+print(person)
+
+========================================================================================
+# Unzip file to temporary directory
+import tempfile
+input_file=r"https://archive.ics.uci.edu/ml/machine-learning-databases/00222/bank.zip"
+
+#create a temporary folder, download the zip file from web, extract the file and read the file into pandas
+
+with tempfile.TemporaryDirectory() as tmpdirname:
+    print('created temporary directory',tmpdirname)
+
+    with requests.get(input_file) as r:
+        with open(f'{tmpdirname}/bank.zip','wb') as f:
+            f.write(r.content)
+
+    with zipfile.ZipFile(f'{tmpdirname}/bank.zip','r') as zip_ref:
+        zip_ref.extractall(path=f'{tmpdirname}') #extract all files into tempdir
+        
+        df=pd.read_csv(f'{tmpdirname}/bank-full.csv')
+        print(df.head())
+min(1,2,3)
+
+========================================================================================
+
 
 
 
